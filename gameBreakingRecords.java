871e788320f553ea7239bbb2ca55fc8890b90ca5
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-05-11 11:43:51 -0500

    Add files via upload

---
+++ gameBreakingRecords.java
@@ -0,0 +1,61 @@
+package gameBreakingRecords;
+
+import java.io.*;
+import java.util.*;
+
+public class gameBreakingRecords {
+
+	/*
+	 * Complete the breakingRecords function below.
+	 */
+	static int[] breakingRecords(int[] s) {
+		{
+			int highest, lowest;
+			highest = lowest = s[0];
+
+			int[] result = new int[2];
+
+			for (int s_i = 1; s_i < s.length; s_i++) {
+				if (s[s_i] > highest) {
+					highest = s[s_i];
+					++result[0];
+				} else if (s[s_i] < lowest) {
+					lowest = s[s_i];
+					++result[1];
+				}
+			}
+
+			return result;
+		}
+	}
+
+	private static final Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter("gameRecord.txt"));
+
+		int n = Integer.parseInt(scan.nextLine().trim());
+
+		int[] score = new int[n];
+
+		String[] scoreItems = scan.nextLine().split(" ");
+
+		for (int scoreItr = 0; scoreItr < n; scoreItr++) {
+			int scoreItem = Integer.parseInt(scoreItems[scoreItr].trim());
+			score[scoreItr] = scoreItem;
+		}
+
+		int[] result = breakingRecords(score);
+
+		for (int resultItr = 0; resultItr < result.length; resultItr++) {
+			bw.write(String.valueOf(result[resultItr]));
+
+			if (resultItr != result.length - 1) {
+				bw.write(" ");
+			}
+		}
+
+		bw.newLine();
+		bw.close();
+	}
+}
