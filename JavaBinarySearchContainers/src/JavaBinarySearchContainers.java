90c2fd1fe9ade6dda3fcb3fa335361d37b3b2c1a
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-06-13 08:18:49 -0500

    Add files via upload

---
+++ JavaBinarySearchContainers/src/JavaBinarySearchContainers.java
@@ -0,0 +1,49 @@
+
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ *  * import java.util.ArrayList;
+ *  
+ *  This program shows that the method contains is much faster for binary search
+ *  then using the method binarySearch. First, the program uses .contains to
+ *  search for the number 100,000. Second, we search for 100,000 using the
+ *  Collections.binarySearch method. We compute time in nanoseconds using
+ *  the method System.nanoTime();
+ *  
+ *  Results are printed to console, which shows that .contains is much faster 
+ *  
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class JavaBinarySearchContainers {
+
+	public static void main(String args[]) {
+
+		// Creating List
+		List<Integer> numbers = new ArrayList<Integer>(10000000); // List of 10M records
+
+		// Initializing List of numbers
+		for (int i = 0; i < numbers.size(); i++) {
+			numbers.add(new Integer(i));
+		}
+
+		// Perform using Java .contains method
+		long startTime = System.nanoTime();
+		numbers.contains(new Integer(5000000)); // Search for 5M number
+		long endTime = System.nanoTime();
+		System.out.println("For 10M records .contains method binary search took " + (endTime - startTime) + " ns");
+
+		// Perform using Java Collections.binarySearch method
+		long startTimeBinary = System.nanoTime();
+		Collections.sort(numbers); // List needs to be sorted for Binary Search
+		Collections.binarySearch(numbers, new Integer(5000000)); // Search for 5M number
+		long endTimeBinary = System.nanoTime();
+		System.out.println(
+				"For 10M records Collections.binarySearch method took " + (endTimeBinary - startTimeBinary) + " ns");
+	}
+
+}
