83c1cd34ce2e5bd9c6e97eac846e9db6d8ab98c8
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-03 08:53:57 -0500

    Create PrimeChecker.java

---
+++ PrimeChecker.java
@@ -0,0 +1,16 @@
+import static java.lang.System.in;
+class Prime {
+    
+    public void checkPrime(int... vars){
+        for(int num : vars){
+            boolean flag = true;
+            if(num<2) flag=false;
+            for(int i=2 ; i<num ; i++){
+                if( num%i == 0)
+                    flag=false;
+            }
+            System.out.print(flag==true?num+" ":"");
+        }
+        System.out.println();
+    }
+}
