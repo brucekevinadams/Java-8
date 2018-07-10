469962eccd4bdb67ebd599a168d3d2ea27d4b798
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-10 16:45:02 -0500

    Add files via upload

--- JavaList/src/JavaList.java
+++ JavaList/src/JavaList.java
@@ -2,63 +2,81 @@
  * Author: Bruce Adams
  * email: ezaroth@gmail.com
  * website: austingamestudios.com
- * 
-This program asks the user to input a number (n), which is the number of integers the user enters on 
-the next line. The numbers are then inserted into a list.
+ *
+This program asks the user how many numbers to enter into an ArrayList. The user then is prompted to either Insert,
+Delete, Print, or Quit. Insert (I) will add a number into the ArrayList, Delete (D), will remove the number from
+the ArrayList, Print will print the ArrayList to the console, and Quit (Q) will quit the program.
 */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class JavaList {
 
-	@SuppressWarnings({ "rawtypes", "unchecked", "boxing" })
-	public static void main(String[] args) {
-		/*
-		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+    @SuppressWarnings({"rawtypes", "unchecked", "boxing"})
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
 		 * class should be named Solution.
 		 */
-		// Let us create a list
-		List list = new ArrayList();
-		// Array for insertion
-		int arr[] = new int[2];
+        // Let us create a list
+        List list = new ArrayList();
+        System.out.println("How many numbers do you want to insert into a list?");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
 
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+        System.out.println("Insert " + n + " numbers.");
+        for (int i = 0; i < n; i++) {
+            list.add(i, scan.nextInt());
+        }
 
-		for (int i = 0; i < n; i++) {
-			list.add(i, scan.nextInt());
-		}
+        System.out.println("List: " + list);
+        String typing = "";
 
-		int queries = scan.nextInt();
+        while (!(typing.equals("Quit") || typing.equals("Q"))) {
+            System.out.println("Type Insert (I), Delete (D), Print (P), or Quit (Q).");
+            switch (typing = scan.next()) {
+                case "Insert":
+                case "I":
+                    System.out.println("Insert a number:");
+                    //int index = scan.nextInt();
+                    int val = scan.nextInt();
+                    System.out.println(val);
+                    //list.add(index, val);
+                    list.add(val);
+                    System.out.println(list);
+                    break;
+                case "Delete":
+                case "D":
+                    System.out.println("Delete a number:");
+                    int del = scan.nextInt();
+                    if (list.contains(del)) {
+                        Object index = list.indexOf(del);
+                        int i = (int) index;
+                       // System.out.println(i);
+                        list.remove(i);
+                    } else
+                        System.out.println("No such number exists in the ArrayList!");
+                    break;
+                case "Print":
+                case "P":
+                    System.out.println(list.toString());
+                    continue;
+                case "Quit":
+                case "Q":
+                case "q":
+                    break;
+                default:
+                    System.out.println("Unknown input");
+                    break;
+            }
+        }
 
-		while (queries > 0) {
-			// System.out.println("Queries = " + queries);
 
-			String com = scan.next();
-			switch (com) {
+        scan.close();
+        System.out.println(list.toString());
+    }
 
-			case "Insert":
-				// System.out.println("Insert called");
-				int index = scan.nextInt();
-				int val = scan.nextInt();
-				list.add(index, val);
-				break;
-
-			case "Delete":
-				// System.out.println("Delete called");
-				int del = scan.nextInt();
-				list.remove(del);
-				break;
-
-			default:
-				break;
-
-			}
-			queries--;
-		}
-
-		scan.close();
-
-		System.out.println(list.toString().replace("[", "").replace("]", "").replace(",", ""));
-	}
 }
+
