4fc524299e4fe46cdb5ff40db5f236cd19544221
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-17 07:34:32 -0500

    Update JavaInterface.java

--- JavaInterface/src/JavaInterface.java
+++ JavaInterface/src/JavaInterface.java
@@ -7,18 +7,22 @@ This program shows how Java Interface can be used with the implements keyword
 to instantiate a public method to call the private interface.
 This is from the Hackerrank problem Java Interface
 */
-import java.util.*;
-interface AdvancedArithmetic{
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+interface AdvancedArithmetic {
     int divisor_sum(int n);
 }
+
 class MyCalculator implements AdvancedArithmetic {
-    public int divisor_sum (int n) {
+    public int divisor_sum(int n) {
         List list = new ArrayList();
         int sum = n;
 
-        for(int i = 1; i <= n / 2; i++)
-        {
-            if(n % i == 0) {
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
                 // Let us create a list
                 list.add(i);
                 sum += i;
@@ -29,24 +33,27 @@ class MyCalculator implements AdvancedArithmetic {
         return sum;
     }
 }
-class JavaInterface{
-    public static void main(String []args){
+
+class JavaInterface {
+    public static void main(String[] args) {
         MyCalculator my_calculator = new MyCalculator();
-        System.out.print("I implemented: ");
+        System.out.println("Abstract Class implemented: ");
         ImplementedInterfaceNames(my_calculator);
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.print("The sum of the divisors of n are: " + my_calculator.divisor_sum(n) + "\n");
+        System.out.print("The sum of the divisors of " + n + " are: " + my_calculator.divisor_sum(n) + "\n");
         sc.close();
     }
+
     /*
      *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
      */
-    static void ImplementedInterfaceNames(Object o){
+    static void ImplementedInterfaceNames(Object o) {
         Class[] theInterfaces = o.getClass().getInterfaces();
-        for (int i = 0; i < theInterfaces.length; i++){
+        for (int i = 0; i < theInterfaces.length; i++) {
             String interfaceName = theInterfaces[i].getName();
             System.out.println(interfaceName);
         }
+        System.out.print("Please enter a number to calculate the Sum of its Divisors: ");
     }
 }
