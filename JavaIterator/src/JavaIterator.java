7e4f7caca7c2a089fad49fc3a386386bb7203dd8
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-17 10:53:45 -0500

    Update JavaIterator.java

--- JavaIterator/src/JavaIterator.java
+++ JavaIterator/src/JavaIterator.java
@@ -5,17 +5,8 @@
  *
 This program shows how iterator and instanceof works and is from a Hackerrank java problem.
 The Class Iterator has the method func which takes the ArrayList mylist as a parameter.
-The Java instanceof operator is used to test if the object or instance is a String. If true,
-it breaks out of the iterator while loop and returns control back to main().
-In this problem we have given you three classes in the editor:
-Student class
-Rockstar class
-Hacker class
-
-In the main method, we populated an ArrayList with several instances of these classes.
-Count method calculates how many instances of each type is present in the ArrayList.
-The code prints three integers, the number of instance of Student class, the number of instance of Rockstar class,
-the number of instance of Hacker class.
+It then compares each element to check if the element is a String, and if so, breaks from
+the loop and returns to main()
 
 Sample Input
 
