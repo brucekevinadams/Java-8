1f3c91035524f07c3fe0f880d2e2850f9fe66f2a
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-17 08:10:57 -0500

    Add files via upload

---
+++ JavaMethodOverriding/src/JavaMethodOverriding.java
@@ -0,0 +1,43 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ *
+This program shows how Java @Override works to override a superclass method with
+a subclass method that is different.
+*/
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports {
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has 11 players in Soccer Class");
+    }
+}
+
+public class JavaMethodOverriding {
+
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+}
