33707dabdbeaa60cd4ec680b36dad7ca4e57b96d
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-18 09:08:34 -0500

    Update JavaExceptionHandling.java

--- JavaExceptionHandling/src/JavaExceptionHandling.java
+++ JavaExceptionHandling/src/JavaExceptionHandling.java
@@ -1,3 +1,32 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ *
+This program shows how Exception Handling in Java works and is from a Hackerrank java problem.
+The Java try/catch block is used for error handling. The code is placed in the try block, and
+if something goes wrong, such as dividing an integer by 0, or the user inputs a string when an integer
+is expected, then the catch block is called.
+
+In this program there are two catch blocks, the first is InputMismatchException, which will catch when non-integers
+are input by the user, such as entering a letter or special character when an int was expected.
+
+Normally, the InputMismatchException block would look like this:
+       } catch (InputMismatchException e) {
+            System.out.println(e);
+       }
+But, in Hackerrank,  this fails Test Case 4, so a hard-coded message is necessary. In the real world, this
+is considered bad practice.
+
+
+Sample Input   Sample Input                                    Sample Input
+10                 10                                              10
+3                   0                                              hello
+
+Sample Output  Sample Output
+3               java.lang.ArithmeticException: / by zero        java.util.InputMismatchException
+*/
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
