6991548a1b341be5566fc45e5273198392104b42
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-07-19 12:50:15 -0500

    Update JavaNotAPower.java

--- JavaNotAPower/src/JavaNotAPower.java
+++ JavaNotAPower/src/JavaNotAPower.java
@@ -1,3 +1,17 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ *
+ Java program where you are given a class Solution and an inner class Inner.Private. The main method
+ of class Solution takes an integer as input. The powerof2 in class Inner.Private checks whether a number
+ is a power of 2. You have to call the method powerof2 of the class Inner.Private from the main method
+ of the class Solution.
+
+ This is from the Hackerrank problem Can You Access?.
+
+*/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.security.Permission;
@@ -12,16 +26,15 @@ public class JavaNotAPower {
             int num = Integer.parseInt(br.readLine().trim());
             Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
 
-            //Write your code here
+            //Code to solve Hackerank problem
             System.out.println(num + " is " + ((Inner.Private) (o = new Inner().new Private())).powerof2(num));
             System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+            // End of code to solve Hackerrank problem
 
-        }//end of try
-
-        catch (DoNotTerminate.ExitTrappedException e) {
+        } catch (DoNotTerminate.ExitTrappedException e) {
             System.out.println("Unsuccessful Termination!!");
         }
-    }//end of main
+    }
 
     static class Inner {
         private class Private {
@@ -29,9 +42,8 @@ public class JavaNotAPower {
                 return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
             }
         }
-    }//end of Inner
-
-}//end of Solution
+    }
+}
 
 class DoNotTerminate { //This class prevents exit(0)
 
