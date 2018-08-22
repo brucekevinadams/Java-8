6fe8022c9066a52836650941c51a6e92549a6815
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-08-22 10:53:39 -0500

    Add files via upload

---
+++ FindDigits/src/FindDigits.java
@@ -0,0 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FindDigits {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        int r = n;
+        int count = 0;
+        while (r > 0) {
+            if (r % 10 != 0 && n % (r % 10) == 0) count++;
+            r = r / 10;
+        }
+        return (count);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String newFile = "output.txt";
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile, true));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            System.out.println(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
