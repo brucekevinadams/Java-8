a5e6a67e79a51ef4debb571349a1c8c0f65cda29
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-08-23 09:09:13 -0500

    Add files via upload

---
+++ RepeatedString/src/RepeatedString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RepeatedString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        long n = in.nextLong();
+        long l = s.length();
+        long a = 0, sum = 0;
+        long temp = n % l;
+        for (long i = 0; i < l; ++i) {
+            if (i < temp && s.charAt((int) i) == 'a') ++sum;
+            if (s.charAt((int) i) == 'a') sum += (n / l);
+        }
+        System.out.println(sum);
+    }
+}
