1552e967c89137fe95af5dc468ffa99a25230a9e
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-08-28 14:51:42 -0500

    Add files via upload

---
+++ StrangeCounter/src/StrangeCounter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class StrangeCounter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long t = in.nextLong(), n = 2;
+        while (3 * (n - 1) < t) n = 2 * n;
+        System.out.println((3 * (n - 1) - t + 1));
+    }
+}
