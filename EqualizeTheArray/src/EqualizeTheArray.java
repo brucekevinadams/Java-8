b4ccd81952a6bb11103181482b33054e1a5578ca
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-08-28 07:18:59 -0500

    Add files via upload

---
+++ EqualizeTheArray/src/EqualizeTheArray.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class EqualizeTheArray {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 0; i < n; i++) al.add(scan.nextInt());
+        HashSet<Integer> hs = new HashSet<>(al);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (Integer i : hs) res.add(Collections.frequency(al, i));
+        System.out.println(n - Collections.max(res, null));
+    }
+}
