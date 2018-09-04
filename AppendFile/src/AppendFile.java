ac1773fb35db2cd662d039051e1ea717e115f3eb
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-04 14:58:54 -0500

    Add files via upload

--- AppendFile/src/AppendFile.java
+++ AppendFile/src/AppendFile.java
@@ -1,11 +1,10 @@
-
 /*
  * Author: Bruce Adams
  * email: ezaroth@gmail.com
  * website: austingamestudios.com
  * 
- Java program to append a string to the
- end of a file.
+ * Java program to append a string to the
+ * end of a file.
 */
 
 import java.io.*;
@@ -13,48 +12,41 @@ import java.util.Scanner;
 
 public class AppendFile {
 
-	public static void appendStrToFile(String fileName, String str) {
-		try {
+    public static void appendStrToFile(String fileName, String str) {
+        try {
 
-			// Open given file in append mode.
-			BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
-			out.write(str);
-			out.close();
-		} catch (IOException e) {
-			System.out.println("exception occoured" + e);
-		}
-	}
-	
+            // Open given file in append mode.
+            BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
+            out.write(str);
+            out.newLine();
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Exception Occurred" + e);
+        }
+    }
+
     private static final Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) throws Exception {
-		// Let us create a sample file with some text
-		String fileName = "output.txt";
-		try {
-			BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
-			out.write("Hello World:\n");
-			out.close();
-		} catch (IOException e) {
-			System.out.println("Exception Occurred" + e);
-		}
+    public static void main(String[] args) throws Exception {
+        // Let us create a sample file with some text
+        String fileName = "output.txt";
 
-		// Let us append given str to above
-		// created file.
+        // Let us append user entered string to above
+        // created file.
         String str = sc.nextLine();
-		appendStrToFile(fileName, str);
+        appendStrToFile(fileName, str);
 
-		// Let us print modified file
-		try {
-			BufferedReader in = new BufferedReader(new FileReader("output.txt"));
+        // Let us print modified file
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("output.txt"));
 
-			String mystring;
-			while ((mystring = in.readLine()) != null) {
-				System.out.println(mystring);
-			}
-			in.close();
-		} catch (IOException e) {
-			System.out.println("Exception Occurred" + e);
-		}
-		
-	}
+            String mystring;
+            while ((mystring = in.readLine()) != null) {
+                System.out.println(mystring);
+            }
+            in.close();
+        } catch (IOException e) {
+            System.out.println("Exception Occurred" + e);
+        }
+    }
 }
