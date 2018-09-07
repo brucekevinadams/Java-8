38eb0addd5ef7ad89ee29247bd2ec9a87e1fafeb
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-07 10:07:14 -0500

    Add files via upload

---
+++ LisasWorkbook/src/LisasWorkbook.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LisasWorkbook {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int chp = sc.nextInt();
+        int prob = sc.nextInt();
+        int sp = 0;
+        int noOfPages = 0;
+        for (int i = 0; i < chp; i++) {
+            int chpProb = sc.nextInt();
+            int pgNo = noOfPages + 1;
+            for (int j = 1; j <= chpProb; j++) {
+                if (j == pgNo)
+                    sp++;
+                if (j % prob == 0 && j < chpProb) {
+                    pgNo++;
+                }
+            }
+            noOfPages = pgNo;
+        }
+        System.out.println(sp);
+    }
+}
