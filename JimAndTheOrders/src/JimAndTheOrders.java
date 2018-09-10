cdf612e6487b564dc172a88a5893fa7ddcc68374
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-10 07:52:16 -0500

    Add files via upload

---
+++ JimAndTheOrders/src/JimAndTheOrders.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class JimAndTheOrders {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < n; i++) {
+            int t = in.nextInt();
+            int d = in.nextInt();
+            int f = t + d;//finish time
+            if (map.containsKey(f)) {
+                List<Integer> list = map.get(f);
+                list.add(i + 1);
+                map.put(f, list);
+            } else {
+                List<Integer> list = new ArrayList<>(Arrays.asList(i + 1));
+                map.put(f, list);//f_time, i-th
+            }
+        }
+        List<Map.Entry<Integer, List<Integer>>> list = new LinkedList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, List<Integer>>>() {
+            public int compare(Map.Entry<Integer, List<Integer>> e1, Map.Entry<Integer, List<Integer>> e2) {
+
+                return e1.getKey().compareTo(e2.getKey());
+            }
+        });
+        for (Map.Entry e : list) {
+            for (Integer i : (List<Integer>) e.getValue()) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
