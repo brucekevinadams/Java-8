669a8fcb5a4bc927482edfbaa53f82df93c3c623
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-11 07:19:05 -0500

    Add files via upload

---
+++ HackerlandRadioTransmitters/src/HackerlandRadioTransmitters.java
@@ -0,0 +1,57 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HackerlandRadioTransmitters {
+
+    // Complete the hackerlandRadioTransmitters function below.
+    static int hackerlandRadioTransmitters(int n, int[] x, int k) {
+        Arrays.sort(x);
+        int numOfTransmitters = 0;
+        int i = 0;
+        while (i < n) {
+            numOfTransmitters++;
+            int loc = x[i] + k;
+            while (i < n && x[i] <= loc) i++;
+            loc = x[--i] + k;
+            while (i < n && x[i] <= loc) i++;
+        }
+        return numOfTransmitters;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String outFile = "output.txt";
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outFile,true));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] x = new int[n];
+
+        String[] xItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int xItem = Integer.parseInt(xItems[i]);
+            x[i] = xItem;
+        }
+
+        int result = hackerlandRadioTransmitters(n, x, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        System.out.println(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
