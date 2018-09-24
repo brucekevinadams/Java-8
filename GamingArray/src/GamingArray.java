9fd80f1bf0c729cb10e81fc8e992625541fdc042
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-24 07:55:20 -0500

    Update GamingArray.java

--- GamingArray/src/GamingArray.java
+++ GamingArray/src/GamingArray.java
@@ -1,3 +1,10 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ * From a Hackerrank problem
+ */
+
 import java.util.Scanner;
 
 public class GamingArray {
