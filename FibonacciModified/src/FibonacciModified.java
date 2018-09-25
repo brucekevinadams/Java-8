085982ba0b9d57f07c4adcb6d3aaeadd8b4a7e5b
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-09-25 07:17:30 -0500

    Add files via upload

---
+++ FibonacciModified/src/FibonacciModified.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FibonacciModified {
+
+    public static void main(String[] args) {
+        int i, n;
+        BigInteger a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextBigInteger();
+        b = sc.nextBigInteger();
+        n = sc.nextInt();
+        BigInteger[] val = new BigInteger[n];
+        val[0] = a;
+        val[1] = b;
+        for (i = 2; i < n; i++) {
+            val[i] = (val[i - 1].pow(2)).add(val[i - 2]);
+        }
+        System.out.println(val[i - 1]);
+    }
+}
