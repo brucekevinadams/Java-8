ccd7a0aec85d50f507da397f5cfaca14def430dd
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-08 07:45:15 -0500

    Add files via upload

---
+++ CorrectnessAndTheLoopInvariant/src/CorrectnessAndTheLoopInvariant.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CorrectnessAndTheLoopInvariant {
+
+    public static void insertionSort(int[] A) {
+        for (int i = 1; i < A.length; i++) {
+            int value = A[i];
+            int j = i;
+            while (j > 0 && A[j - 1] > value) {
+                A[j] = A[j - 1];
+                j--;
+            }
+            A[j] = value;
+        }
+        for (int i = 0; i < A.length; i++)
+            System.out.print(A[i] + " ");
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = in.nextInt();
+        }
+        insertionSort(ar);
+    }
+}
