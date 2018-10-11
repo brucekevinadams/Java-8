2f2d0e3daf912cf17ec67ef2d8aa94cc70a24883
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-11 08:50:31 -0500

    Add files via upload

---
+++ StringConstruction/src/StringConstruction.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StringConstruction {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int a0 = 0; a0 < n; a0++) {
+            String s = in.next();
+            System.out.println(s.chars().distinct().count());
+        }
+    }
+
+}
