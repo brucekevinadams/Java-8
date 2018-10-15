4aba718237a794e85e938ccf39c63de39330a83d
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-15 07:33:39 -0500

    Update PalindromeIndex.java

--- PalindromeIndex/src/PalindromeIndex.java
+++ PalindromeIndex/src/PalindromeIndex.java
@@ -4,6 +4,12 @@
  * website: austingamestudios.com
  * Java 8 program
  * Hackerrank problem
+ * Given a string of lowercase letters in the range ascii[a-z], 
+ * determine a character that can be removed to make the string a palindrome.
+ * There may be more than one solution, but any will do. For example, if your
+ * string is "bcbc", you can either remove 'b' at index 0 or 'c' at index 3.
+ * If the word is already a palindrome or there is no solution, return -1. 
+ * Otherwise, return the index of a character to remove. 
 */
 
 import java.io.BufferedWriter;
