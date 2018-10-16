cd5df1c619066a68096025909b9d41ba9809bbf1
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-16 08:26:51 -0500

    Update JavaReadTextFile.java

--- JavaReadTextFile/src/JavaReadTextFile.java
+++ JavaReadTextFile/src/JavaReadTextFile.java
@@ -8,16 +8,18 @@ import java.io.FileReader;
 public class JavaReadTextFile {
     public static void main(String[] args) throws Exception {
         FileDialog dialog = new FileDialog((Frame) null, "Select File to Open");
+        System.out.println("Select file to open");
         dialog.setMode(FileDialog.LOAD);
         dialog.setVisible(true);
         String filename = dialog.getFile();
 
         String path = dialog.getDirectory() + dialog.getFile();
         System.out.println("File Path: " + path);
-        System.out.println("File: " + filename);
+        System.out.println("File: " + filename + "\n");
 
         BufferedReader input = new BufferedReader(new FileReader(filename));
         String st;
+        System.out.println("< CONTENTS OF FILE >");
         while ((st = input.readLine()) != null)
             System.out.println(st);
 
