173994169c42254420064b22b87e4a047463e2a3
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-17 07:29:16 -0500

    Add files via upload

---
+++ MaximizingXOR/src/MaximizingXOR.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MaximizingXOR {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int L = scan.nextInt();
+        int R = scan.nextInt();
+        scan.close();
+
+        int xored = L ^ R;
+        int significantBit = 31 - Integer.numberOfLeadingZeros(xored);
+        int result = (1 << (significantBit + 1)) - 1;
+
+        System.out.println(result);
+    }
+}
+
