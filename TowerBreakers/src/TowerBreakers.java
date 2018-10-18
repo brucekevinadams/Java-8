eafaf168e1a4a214eefc37a152b9abb84a513c50
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-18 07:53:58 -0500

    Add files via upload

---
+++ TowerBreakers/src/TowerBreakers.java
@@ -0,0 +1,42 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TowerBreakers {
+
+    // Complete the towerBreakers function below.
+    static int towerBreakers(int n, int m) {
+        int answer = (m == 1 || n % 2 == 0 ? 2 : 1);
+        return answer;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String output = "output.txt";
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output, true));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nm = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nm[0]);
+
+            int m = Integer.parseInt(nm[1]);
+
+            int result = towerBreakers(n, m);
+
+            bufferedWriter.write(String.valueOf(result));
+            System.out.println(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
