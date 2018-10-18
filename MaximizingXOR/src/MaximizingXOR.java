319ad0c4f64c88c9f6c2b89552f01722ff0b7720
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-18 08:33:51 -0500

    Update MaximizingXOR.java

--- MaximizingXOR/src/MaximizingXOR.java
+++ MaximizingXOR/src/MaximizingXOR.java
@@ -4,6 +4,9 @@
  * website: austingamestudios.com
  * Java 8 program
  * Given two integers, L and R, find the maximal value of xor L and R 
+ * The program runs between the numbers, starting at L and ending at R
+ * For example, if L is 2 and R is 5, the program XOR's 2 and 3, 2 and 4 and
+ * 2 and 5, to find the largest XOR result, which is then print out to console
  */
 
 import java.util.Scanner;
