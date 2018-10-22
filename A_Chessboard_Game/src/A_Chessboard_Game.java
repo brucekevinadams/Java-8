d1d69c4779bb02e83d4ab53eb5c5e3be2748f289
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-10-22 08:53:24 -0500

    Add files via upload

---
+++ A_Chessboard_Game/src/A_Chessboard_Game.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class A_Chessboard_Game {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            if ((x % 4 == 1 || x % 4 == 2) && (y % 4 == 1 || y % 4 == 2)) {
+                System.out.println("Second");
+            } else {
+                System.out.println("First");
+            }
+        }
+    }
+}
